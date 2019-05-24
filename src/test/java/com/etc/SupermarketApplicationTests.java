ab8577bed68ac0c33860f26b08e28af23a0c0604
jljljf <945448835@qq.com>
2019-05-24 08:47:11 +0800

    点击二级目录可跳转的界面

--- src/test/java/com/etc/SupermarketApplicationTests.java
+++ src/test/java/com/etc/SupermarketApplicationTests.java
@@ -1,7 +1,10 @@
 package com.etc;
 
+import com.etc.service.OneLevelService;
+import com.etc.service.ProductDisplayService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
