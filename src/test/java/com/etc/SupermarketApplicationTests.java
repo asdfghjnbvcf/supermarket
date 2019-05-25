e416aa9cd299d6de9f1ad5b0e6fef9814994c693
小吴 <791988557@qq.com>
2019-05-25 13:31:59 +0800

    实体类、dao/xml

--- src/test/java/com/etc/SupermarketApplicationTests.java
+++ src/test/java/com/etc/SupermarketApplicationTests.java
@@ -1,29 +1,37 @@
 package com.etc;
 
 
-import com.etc.dao.TwoLevelDao;
+
+import com.etc.dao.*;
+import com.etc.entity.FlashSale;
+import com.etc.entity.po.FlashGoodsMessagePo;
+import com.etc.entity.po.FlashGoodsNumPo;
+import com.etc.redis.FlashGoodsKey;
+import com.etc.redis.RedisService;
 import com.etc.service.GoodsService;
 import com.etc.service.TwoLevelService;
+import com.etc.service.impl.FlashGoodsServiceImpl;
+import com.etc.service.impl.FlashSaleServiceImpl;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SupermarketApplicationTests {
-    @Autowired
-    private TwoLevelService twoLevelService;
-    @Autowired
-    private TwoLevelDao twoLevelDao;
-    @Autowired
-    private GoodsService goodsService;
+
+
     @Test
-    public void contextLoads () {
-        goodsService.listGoodsByPage(1,5,"").getData().forEach(System.out::println);
+    public void testredis(){
 
     }
+
+
 
 
 }
